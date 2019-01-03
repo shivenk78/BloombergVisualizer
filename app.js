8bddedbf49665c381ddbfb33e863d80624b3eaf7
Shiven <kumar.shiven15@gmail.com>
2019-01-03 00:13:02 -0500

    Working visualizer, need to fix sizes and add a GUI

--- app.js
+++ app.js
@@ -19,17 +19,15 @@ function createScene() {
                 uptimePerArea.push(0);
         }
 
-        // Create the scene space
         var scene = new BABYLON.Scene(engine);
-        var pilot = new BABYLON.Vector3(0,0,0);
-
-        // Add a camera to the scene and attach it to the canvas
-        var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0,0,5), scene);
+        var light = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(0, -0.5, 1.0), scene);
+        var camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, BABYLON.Vector3.Zero(), scene);
+        camera.setPosition(new BABYLON.Vector3(20, 70, -100));
+        light.position = new BABYLON.Vector3(0, 25, -50);
         camera.attachControl(canvas, true);
 
         // Add lights to the scene
         var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
-        var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
         
         $.getJSON("https://shivenk78.github.io/BloombergVisualizer/SBHSData.json", function(data){
                 $.each(data, function (index, value) {
@@ -52,14 +50,135 @@ function createScene() {
                         uptimePerVersion[uniqueVersions.indexOf(value.version)] += parseInt(value.uptime, 10);
                 });
 
-                var count = 0;
+                /*var count = 0;
                 for(var i = 0; i < uptimePerArea.length; i++){
                         var box = BABYLON.MeshBuilder.CreateBox("box", {height: parseInt(uptimePerArea[i]/10000)+0.001, width:10, depth:10}, scene);
                         box.position = new BABYLON.Vector3(20*count, uptimePerArea[i]/20000, 0);
                         console.log(uptimePerArea[i]);
                         count++;
-                }
+                }*/
+
+                // Data
+        var scale = 0.6;
+        var areaSeries = [];
+        for(var i = 0; i<uptimePerArea.length; i++){
+                var colorMod = 255-(i*(255/uptimePerArea.length));
+                console.log(colorMod);
+                areaSeries.push({label: uniqueAreas[i], value: uptimePerArea[i]/5000, color: new BABYLON.Color3(colorMod, 0, 0)});
+        }
+    
+        var versionSeries = [];
+        for(var i = 0; i<uptimePerVersion.length; i++){
+                var colorMod = 255-(i*(255/uptimePerVersion.length));
+                versionSeries.push({label: uniqueVersions[i], value: uptimePerVersion[i]/5000, color: new BABYLON.Color3(0, colorMod, 0)});
+        }
+        
+        var browsers_Series = [
+                { label: "IE", value: 32, color: new BABYLON.Color3(0, 0, 1) },
+                { label: "Chrome", value: 28, color: new BABYLON.Color3(1, 0, 0) },
+                { label: "Firefox", value: 16, color: new BABYLON.Color3(1, 0, 1) },
+                { label: "Opera", value: 14, color: new BABYLON.Color3(1, 1, 0) },
+                { label: "Safari", value: 10, color: new BABYLON.Color3(0, 1, 1) }        
+            ];
+
+    var playgroundSize = 200;
+    // Background
+    var background = BABYLON.Mesh.CreatePlane("background", playgroundSize, scene, false);
+    background.material = new BABYLON.StandardMaterial("background", scene);
+    background.scaling.y = 0.5;
+    background.position.z = playgroundSize / 2 - 0.5;
+    background.position.y = playgroundSize / 4;
+    background.receiveShadows = true;
+    var backgroundTexture = new BABYLON.DynamicTexture("dynamic texture", 512, scene, true);
+    background.material.diffuseTexture = backgroundTexture;
+    background.material.specularColor = new BABYLON.Color3(0, 0, 0);
+    background.material.backFaceCulling = false;
+
+    backgroundTexture.drawText("Bloomberg", null, 80, "bold 70px Segoe UI", "white", "#555555");
+    backgroundTexture.drawText("Uptime per Area", null, 250, "35px Segoe UI", "white", null);
+
+    // Ground    
+    var ground = BABYLON.Mesh.CreateGround("ground", playgroundSize, playgroundSize, 1, scene, false);
+    var groundMaterial = new BABYLON.StandardMaterial("ground", scene);
+    groundMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+    groundMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    ground.material = groundMaterial;
+    ground.receiveShadows = true;
+    ground.position.y = -0.1;
+    
+    var shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
+	shadowGenerator.usePoissonSampling = true;
+    
+    var createSeries = function (series) {
+        var margin = 0.1;
+        var offset = playgroundSize / (series.length) - margin;
+        var x = -playgroundSize / 2 + offset / 2;
+
+        for (var index = 0; index < series.length; index++) {
+            var data = series[index];
+
+            var bar = BABYLON.Mesh.CreateBox(data.label, 1.0, scene, false);
+            bar.scaling = new BABYLON.Vector3(offset / 2.0, 0, offset / 2.0);
+            bar.position.x = x;
+            bar.position.y = 0;
+            
+            // Animate a bit
+            var animation = new BABYLON.Animation("anim", "scaling", 30, BABYLON.Animation.ANIMATIONTYPE_VECTOR3);
+            animation.setKeys([
+                { frame: 0, value: new BABYLON.Vector3(offset / 2.0, 0, offset / 2.0) },
+                { frame: 100, value: new BABYLON.Vector3(offset / 2.0, data.value * scale, offset / 2.0) }]);
+            bar.animations.push(animation);
+            
+            animation = new BABYLON.Animation("anim2", "position.y", 30, BABYLON.Animation.ANIMATIONTYPE_FLOAT);
+            animation.setKeys([
+                { frame: 0, value: 0 },
+                { frame: 100, value: (data.value * scale) / 2 }]);
+            bar.animations.push(animation);            
+            scene.beginAnimation(bar, 0, 100, false, 2.0);
+
+            // Material
+            bar.material = new BABYLON.StandardMaterial(data.label + "mat", scene);
+            bar.material.diffuseColor = data.color;
+            bar.material.emissiveColor = data.color.scale(0.3);
+            bar.material.specularColor = new BABYLON.Color3(0, 0, 0);
+
+            // Shadows
+            shadowGenerator.getShadowMap().renderList.push(bar);
+                        
+            // Legend
+            var barLegend = BABYLON.Mesh.CreateGround(data.label + "Legend", playgroundSize / 2, offset * 2, 1, scene, false);
+            barLegend.position.x = x;
+            barLegend.position.z = -playgroundSize / 4;
+            barLegend.rotation.y = Math.PI / 2;
+            
+            barLegend.material = new BABYLON.StandardMaterial(data.label + "LegendMat", scene);
+            var barLegendTexture = new BABYLON.DynamicTexture("dynamic texture", 512, scene, true);
+            barLegendTexture.hasAlpha = true;
+            barLegend.material.diffuseTexture = barLegendTexture;
+            barLegend.material.emissiveColor = new BABYLON.Color3(0.4, 0.4, 0.4);
+            
+            var size = barLegendTexture.getSize();
+            barLegendTexture.drawText(data.label + " (" + data.value + "%)", 80, size.height / 2 + 30, "bold 50px Segoe UI", "white", "transparent");
+            
+            // Going next
+            x += offset + margin;
+        }
+    };
+
+    createSeries(areaSeries);
         });
+
+        camera.attachControl(canvas, true);
+
+
+    // Limit camera
+    camera.lowerAlphaLimit = Math.PI;
+    camera.upperAlphaLimit = 2 * Math.PI;
+    camera.lowerBetaLimit = 0.1;
+    camera.upperBetaLimit = (Math.PI / 2) * 0.99;
+    camera.lowerRadiusLimit = 5;
+    camera.upperRadiusLimit = 15000;
+
 
         return scene;
 };
